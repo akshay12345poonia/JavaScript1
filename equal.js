b2ac7a50a9aee4de66cfe7dedd0bddca75f906d9
akshay Poonia <akshay12345poonia@gmail.com>
2024-10-25 23:17:49 +0530

    added new file

---
+++ equal.js
@@ -0,0 +1,20 @@
+let num1 = 10;
+let num2 = 5;
+
+//Perform mathematical Operations
+let add = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+
+//Compare values
+let isGreater = num1 > num2;
+let isEqualStrict = num1 === num2;
+let isEqualLoose = num1 == num2;
+
+//Display Result
+console.log("addition:", add);
+console.log("subtraction:", sub);
+console.log("Multiplication:", mul);
+console.log("num1 is greater than num2:", isGreater);
+console.log("num1 is strictly equal to num2:", isEqualStrict);
+console.log("num1 is loosely equal to num2", isEqualLoose);
